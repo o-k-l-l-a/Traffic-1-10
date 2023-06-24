6e641f8c4875419fd48a5c4dc6134fae710d6512
alireza ezzat <89906412+alirezaezzatofficial@users.noreply.github.com>
2023-06-24 17:11:54 +0330

    Update beta.Traffic-1-10.py

--- beta.Traffic-1-10.py
+++ beta.Traffic-1-10.py
@@ -20,12 +20,21 @@ def get_upload_speed(interface):
     upload_speed = (new_value - old_value) / 1024 / 1024 * 8 # in Mbps
     return upload_speed
 
-# Set the target address and port for sending packets
-ip = "1.1.1.1"
-port = 443
-
+# Ask for the network interface name and protocol type
 interface = input("Enter network interface name (e.g. en0): ")
-DEFAULT_DATA_SIZE = get_download_speed(interface) * 10
+protocol_type = input("Enter the protocol type (TCP/UDP): ")
+
+# Set the target address and port for sending packets based on the protocol type
+if protocol_type.lower() == "tcp":
+    ip = "178.22.122.100"
+    port = 53
+elif protocol_type.lower() == "udp":
+    ip = "185.51.200.2"
+    port = 53
+
+# Ask for the packet size multiplier
+multiplier = int(input("Enter multiplier value (e.g. 20): "))
+DEFAULT_DATA_SIZE = get_download_speed(interface) * multiplier
 data_size_mbit = DEFAULT_DATA_SIZE
 
 # Create a table to show speed and data usage statistics
@@ -51,18 +60,27 @@ while True:
 
     print(table)
 
-    # Create a TCP socket
-    sock = socket.socket()
-    sock.connect((ip, port))
+    # Create a socket based on the protocol type
+    if protocol_type.lower() == "tcp":
+        sock = socket.socket()
+        sock.connect((ip, port))
+    elif protocol_type.lower() == "udp":
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     # Send the data with the requested size
     data_size_byte = int(data_size_mbit * 1024 * 1024 / 8)
-    payload = b'a' * data_size_byte
-    sock.sendall(payload)
+    mtu = 1400 # Change the value based on your network MTU
+    num_packets = data_size_byte // mtu + 1
+    for i in range(num_packets):
+        start_idx = i * mtu
+        end_idx = start_idx + mtu if i < num_packets - 1 else data_size_byte
+        payload = b'a' * (end_idx - start_idx)
+        if protocol_type.lower() == "tcp":
+            sock.sendall(payload)
+        elif protocol_type.lower() == "udp":
+            sock.sendto(payload, (ip, port))
 
     print("Packet sent to {}:{}".format(ip, port))
     
     # Wait for some time before next calculation and packet transmission
     time.sleep(1)
-# pip install psutil
-# pip install prettytable
